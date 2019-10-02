05ce0fd3629b6148f5be28ab6d537123a0398cfb
Thanawat <thanawat.bcr@gmail.com>
2019-10-02 15:04:00 +0700

    Add home page!

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const port = process.env.PORT || 3000
 const app = express()
 require('dotenv').config()
 
+app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 })
 
 app.listen(port, () => console.log(`Running on ${port}`))
